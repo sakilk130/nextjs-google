65360a2240501432ff3e5591ccd1f731f297e5c0
sakilk130 <sakilk130@gmail.com>
2021-12-03 17:30:19 +0600

    create body

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  future: {
+    webpack5: true,
+  },
   reactStrictMode: true,
-}
+  images: {
+    domains: ['www.google.co.uk'],
+  },
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Avatar from '../components/Avatar/Avatar';
-import { ViewGridIcon } from '@heroicons/react/solid';
+import {
+  MicrophoneIcon,
+  SearchIcon,
+  ViewGridIcon,
+} from '@heroicons/react/solid';
+import Image from 'next/image';
 
 const Home: NextPage = () => {
   return (
@@ -23,6 +28,23 @@ const Home: NextPage = () => {
           <Avatar link="https://avatars.githubusercontent.com/u/44520484?v=4" />
         </div>
       </header>
+      <form className="flex flex-col items-center mt-44 flex-grow">
+        <Image
+          loading="lazy"
+          src="https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+          height={100}
+          width={300}
+        />
+        <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl">
+          <SearchIcon className="h-5 mr-3 text-gray-500" />
+          <input type="text" className="flex-grow focus:outline-none" />
+          <MicrophoneIcon className="h-5" />
+        </div>
+        <div className="flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4">
+          <button className="btn">Google Search</button>
+          <button className="btn">I&apos;m Feeling Lucky</button>
+        </div>
+      </form>
     </div>
   );
 };
